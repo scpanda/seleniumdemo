406353b12862045b16a6a6fb4d8a9592f2f97a41
frank xiong <scpanda198727@gmail.com>
2014-10-23 01:28:28 +0800

    create program

---
+++ SeleniumDemo/src/selenium/test/SeleniumDemo.java
@@ -0,0 +1,29 @@
+package selenium.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://cn.bing.com");
+		driver.manage().window().maximize();
+		System.out.println("first title is:"+driver.getTitle());
+		
+		WebElement input_area=driver.findElement(By.name("q"));
+		input_area.sendKeys("¾©¶«");
+		
+		WebElement search_buuton=driver.findElement(By.name("go"));
+		search_buuton.click();
+		System.out.println("now title is:"+driver.getTitle());
+		
+		//¹Ø±Õä¯ÀÀÆ÷
+		//driver.quit();
+	}
+
+}
